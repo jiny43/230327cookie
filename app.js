9b8917258c752d70f498c814fb62e650fab1cfa0
jieun <je_eun2735@naver.com>
2023-03-27 17:07:37 +0900

    cookie-parser 미들웨어 설치

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const credentials = require('./.credentials.development')
 
 const cookieParser = require('cookie-parser');
-const { appendFile } = require('fs');
 appendFile.use(cookieParser(credentials.cookieSecret))
